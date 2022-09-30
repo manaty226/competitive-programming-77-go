9b8a86d3778c8ca71abb8616729a3456eba5c95a
manaty226 <ta.manaty0226@gmail.com>
2022-09-30 21:53:16 +0900

    worklog Sept. 30

---
+++ sec2/a08.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Scanner struct {
+	*bufio.Scanner
+}
+
+func NewScanner() *Scanner {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	return &Scanner{scanner}
+}
+
+func (s *Scanner) NextInt() int {
+	s.Scan()
+	word := s.Text()
+	n, _ := strconv.Atoi(word)
+	return n
+}
+
+func main() {
+	s := NewScanner()
+
+	H := s.NextInt()
+	W := s.NextInt()
+
+	dim := make([][]int, H+1)
+	dim[0] = make([]int, W+1)
+	for i := 1; i <= H; i++ {
+		dim[i] = make([]int, W+1)
+		for j := 1; j <= W; j++ {
+			dim[i][j] = s.NextInt() + dim[i][j-1]
+		}
+	}
+
+	for i := 1; i <= H; i++ {
+		for j := 1; j <= W; j++ {
+			dim[i][j] += dim[i-1][j]
+		}
+	}
+
+	Q := s.NextInt()
+	for i := 0; i < Q; i++ {
+		A := s.NextInt()
+		B := s.NextInt()
+		C := s.NextInt()
+		D := s.NextInt()
+
+		res := dim[A-1][B-1] + dim[C][D] - dim[A-1][D] - dim[C][B-1]
+		fmt.Printf("%d \n", res)
+	}
+}
