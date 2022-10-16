d4bd1be9a0c1da6babc056c34caefc3ca995b0e3
manaty226 <ta.manaty0226@gmail.com>
2022-10-16 09:40:31 +0900

    worklog Oct. 16

---
+++ sec5/a27.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Scanner struct {
+	*bufio.Scanner
+}
+
+func NewScanner() *Scanner {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	return &Scanner{scanner}
+}
+
+func (s *Scanner) NextInt() int {
+	s.Scan()
+	word := s.Text()
+	n, _ := strconv.Atoi(word)
+	return n
+}
+
+func Gcd(x, y int) int {
+	a, b := x, y
+	if x > y {
+		a, b = y, x
+	}
+	residual := b % a
+	if residual == 0 {
+		return a
+	}
+	return Gcd(a, residual)
+}
+
+func main() {
+	s := NewScanner()
+	A, B := s.NextInt(), s.NextInt()
+
+	gcd := Gcd(A, B)
+	fmt.Printf("%d \n", gcd)
+}
