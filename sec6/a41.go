2b7410c1d8daaaebb22f686193675b0c46113988
manaty226 <ta.manaty0226@gmail.com>
2022-11-03 10:08:24 +0900

    worklog Dec. 3

---
+++ sec6/a41.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Scanner struct {
+	*bufio.Scanner
+	*bufio.Reader
+}
+
+func NewScanner() *Scanner {
+	scanner := bufio.NewScanner(os.Stdin)
+	buf := make([]byte, 10000)
+	scanner.Buffer(buf, 256*1000)
+
+	reader := bufio.NewReader(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	return &Scanner{scanner, reader}
+}
+
+func (s *Scanner) NextInt() int {
+	s.Scan()
+	word := s.Text()
+	n, _ := strconv.Atoi(word)
+	return n
+}
+
+func (s *Scanner) NextString() string {
+	s.Scan()
+	return s.Text()
+}
+
+func maxInts(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func main() {
+	s := NewScanner()
+	N := s.NextInt()
+	S := s.NextString()
+
+	answer := false
+	for i := 0; i < N-2; i++ {
+		if (S[i] == S[i+1]) && (S[i+1] == S[i+2]) {
+			answer = true
+		}
+	}
+	if answer {
+		fmt.Printf("Yes \n")
+	} else {
+		fmt.Printf("No \n")
+	}
+}
---
+++ sec6/b41.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Scanner struct {
+	*bufio.Scanner
+	*bufio.Reader
+}
+
+func NewScanner() *Scanner {
+	scanner := bufio.NewScanner(os.Stdin)
+	buf := make([]byte, 10000)
+	scanner.Buffer(buf, 256*1000)
+
+	reader := bufio.NewReader(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	return &Scanner{scanner, reader}
+}
+
+func (s *Scanner) NextInt() int {
+	s.Scan()
+	word := s.Text()
+	n, _ := strconv.Atoi(word)
+	return n
+}
+
+func (s *Scanner) NextString() string {
+	s.Scan()
+	return s.Text()
+}
+
+func maxInts(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func main() {
+	s := NewScanner()
+	X, Y := s.NextInt(), s.NextInt()
+	answer := [][2]int{[2]int{X, Y}}
+
+	if X == 1 && Y == 1 {
+		fmt.Printf("%d \n", 0)
+		return
+	}
+
+	for X > 0 && Y > 0 {
+		if X > Y {
+			X -= Y
+		} else {
+			Y -= X
+		}
+		if X == 1 && Y == 1 {
+			break
+		}
+		answer = append(answer, [2]int{X, Y})
+	}
+	fmt.Printf("%d \n", len(answer))
+
+	for i := len(answer) - 1; i >= 0; i-- {
+		fmt.Printf("%d %d \n", answer[i][0], answer[i][1])
+	}
+
+}
